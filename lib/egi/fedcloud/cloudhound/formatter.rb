244e1b67ff1c58eab357034811b11e202b3d8469
Boris Parak <parak@cesnet.cz>
2015-05-28 11:17:15 +0200

    Added table formatting

--- lib/egi/fedcloud/cloudhound/formatter.rb
+++ lib/egi/fedcloud/cloudhound/formatter.rb
@@ -5,10 +5,24 @@ class Egi::Fedcloud::Cloudhound::Formatter
 
   class << self
     #
-    def as_table(data = {})
+    def as_table(data = [])
       # TODO: use terminal-table
       Egi::Fedcloud::Cloudhound::Log.debug "[#{self}] Transforming #{data.inspect} into a table"
-      data.to_s
+      table = Terminal::Table.new
+
+      table.add_row [' >>> Site Name <<< ', ' >>> NGI Name <<< ', ' >>> CSIRT Contact(s) <<< ']
+      table.add_separator
+      data.each do |site|
+        table.add_separator
+        table.add_row [site[:name], site[:ngi], site[:csirt]]
+      end
+
+      table
+    end
+
+    #
+    def as_json(data = [])
+      data ? JSON.generate(data) : '{}'
     end
   end
 
